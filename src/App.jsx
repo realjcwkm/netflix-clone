644f5645c4383128c14be627882ad6856516948b
realjcwkm <realjcwkm@gmail.com>
2021-08-23 17:35:45 -0400

    fix App calling all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,85 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
+import './App.css';
 
+/* Header
+Destaque
+Listas
+Rodape */
 function App() {
+    const [movieList, setMovieList] = useState([]);
+    const [featureData, setFeatureData] = useState(null);
+    const [blackHeader, setBlackHeader] = useState(false);
 
-  const [movieList, setMovieList] = useState([]);
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
 
-  useEffect(()=>{
-    const loadAll = async () => {
-      let list = await Tmdb.getHomeList();
-      setMovieList(list);
-    }
+            //pegando o filme em destaque
+            let originals = list.filter((i) => i.slug === 'originals');
+            let randomChosen = Math.floor(
+                Math.random() * originals[0].items.results.length - 1
+            );
+            let chosen = originals[0].items.results[randomChosen];
 
-    loadAll();
-  }, []);
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
 
-  return(
-    <div className="page">
-      Header
-      Destaque
-      Listas
-      Rodape
-    </div>
-  );
+            setFeatureData(chosenInfo);
+        };
+
+        loadAll();
+    }, []);
+
+    useEffect(() => {
+        const scrollListener = () => {
+            if (window.scrollY > 10) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
+        };
+        window.addEventListener('scroll', scrollListener);
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        };
+    }, []);
+
+    return (
+        <div className="page">
+            <Header black={blackHeader} />
+
+            {featureData && <FeatureMovie item={featureData} />}
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
+
+            <footer>
+                Feito com{' '}
+                <span role="img" aria-label="coração">
+                    ❤️
+                </span>{' '}
+                pela B7Web <br />
+                Direitos de imagem para Netflix <br />
+                Dados do site Themoviedb.org
+                <br />
+            </footer>
+
+            {movieList.length <= 0 && (
+                <div className="loading">
+                    <img
+                        src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+                        alt="carregando"
+                    />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
